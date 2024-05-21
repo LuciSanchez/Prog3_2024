a99e6a6a47d966593053342dce20bd6122c75546
lucia <lucii.sanchez06@gmail.com>
2024-05-21 18:53:02 -0300

    Creamos la clase student y el metodo agregar

---
+++ model/student.class.php
@@ -0,0 +1,65 @@
+<?php
+include("database.class.php");
+
+class Student{
+    private $idStudent;
+    private $dni=null;
+    private $surname=null;
+    private $name=null;
+    private $birthdate=null;
+    private $phone=null;
+    private $address=null;
+    private $email=null;
+    private $password=null;
+    private $school=null;
+    private $conexion=null;
+    //geter y seter
+
+    public function __construct($dni, $surname, $name, $birthdate, $phone, $address, $email, $password, $school, ){
+        $this->dni=$dni;
+        $this->surname=$surname;
+        $this->name=$name;
+        $this->birthdate=$birthdate;
+        $this->phone=$phone;
+        $this->address=$address;
+        $this->email=$email;
+        $this->password=$password;
+        $this->school=$school;
+    }
+
+    public function addStudent(){
+    //crear la consulta
+    $sql="INSERT INTO students (dni, surname, name, birthdate, phone, address, email, password, school) VALUES 
+    (".$this->dni.",'".$this->surname."', '".$this->name."', '".$this->birthdate."', '".$this->phone."', '".$this->address."', '".$this->email."', '".$this->password."', '".$this->school."',)";
+
+    $this->conexion=new Database();
+    $result= $this->conexion->query($sql);
+    $this->conexion->close();
+
+    return $result;
+    }
+
+    public function editStudent(){
+
+    }
+
+    public function deleteStudent(){
+        
+    }
+
+    public function getStudent(){
+        
+    }
+
+    public function getALLStudent(){
+        
+    }
+
+    public function getIdStudent(){
+        return $this->idStudent;
+    }
+    public function setIdStudent (){
+        $this->idStudent=$idStudent;
+    }
+
+}
